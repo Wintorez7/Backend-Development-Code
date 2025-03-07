039b965aac915f396c3b0876bcbc86c4aeba1ef1
Wintorez7 <liordx76@gmail.com>
2025-03-07 19:08:37 +0530

    learning CRUD operation

--- Todo-App-1/controller/user.js
+++ Todo-App-1/controller/user.js
@@ -1,3 +1,5 @@
+import { User } from "../models/user";
+import bcrypt from "bcrypt"
 
 export const register = async (req,res) => {
     try {
@@ -8,7 +10,29 @@ export const register = async (req,res) => {
             message:"All Fields are Required"
         })
        }
-         
+      
+       // finding user are already Register or not   
+       const User = await User.findOne(email);
+       if(User){
+        return res.status(403).json({
+            success: false,
+            message: "this email Id already register"
+        })
+       }
+
+       const hashedPassowrd = await bcrypt.hash(password,10);
+
+       await User.create({
+            fullName,
+            email,
+            password:hashedPassowrd
+       })
+
+       return res.status(201).json({
+        success:true,
+        message:"Account Created successfully"
+       })
+
     } catch (error) {
         console.log(error)
     }
