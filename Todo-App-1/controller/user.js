248ae6938e69a0effba1dddf0e1bdb6b77bceae2
Wintorez7 <liordx76@gmail.com>
2025-03-11 13:08:28 +0530

    adding Headers

--- Todo-App-1/controller/user.js
+++ Todo-App-1/controller/user.js
@@ -1,39 +1,83 @@
-import { User } from "../models/user";
+import { User } from "../models/user.js";
 import bcrypt from "bcrypt"
 
 export const register = async (req,res) => {
     try {
        const {fullName,email,password} = req.body;
        if(!fullName || !email || !password){
-        return res.status(430).json({
-            success:false,
-            message:"All Fields are Required"
-        })
+        return res.status(400).json({
+            success: false,
+            message: "All Fields are Required"
+        });        
        }
       
        // finding user are already Register or not   
-       const User = await User.findOne(email);
-       if(User){
-        return res.status(403).json({
-            success: false,
-            message: "this email Id already register"
+      const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(403).json({
+                success: false,
+                message: "This email ID is already registered"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+                fullName,
+                email,
+                password:hashedPassword
         })
-       }
 
-       const hashedPassowrd = await bcrypt.hash(password,10);
-
-       await User.create({
-            fullName,
-            email,
-            password:hashedPassowrd
-       })
-
-       return res.status(201).json({
-        success:true,
-        message:"Account Created successfully"
-       })
+        return res.status(201).json({
+            success:true,
+            message:"Account Created successfully"
+        })
 
     } catch (error) {
-        console.log(error)
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export const login = async (req,res) => {
+    try {
+        const {email , password} = req.body;
+        if(!email || !password){
+            return res.status(403).json({
+                success:false,
+                message:"All fileds are required"
+            })
+        }
+
+        const LoginUser = await User.findOne({ email });
+        if (!LoginUser) {
+            return res.status(403).json({
+                success: false,
+                message: "incorrect email password"
+            });
+        }
+
+        const isPasswordIsMatched = await bcrypt.compare(password , LoginUser.password)
+        if(!isPasswordIsMatched){
+            return res.status(403).json({
+                success: false,
+                message: "incorrect email password"
+            });
+        }
+
+        return res.status(201).json({
+            success:true,
+            message:`Welcome Back ${LoginUser.fullName}`
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            success:false,
+            message:"Something Went Wrong try agin later"
+        });
     }
 }
--- Todo-App-1/index.js
+++ Todo-App-1/index.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import 'dotenv/config';
 import connectToDb from "./database/database.js"
+import userRoute from "./routes/user.js"
+import bodyParser from 'body-parser';
 const app = express()
 
 connectToDb()
+
+// Middlewares
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use("/api/v1/user", userRoute); 
 
 const PORT = process.env.PORT || 4000
 
