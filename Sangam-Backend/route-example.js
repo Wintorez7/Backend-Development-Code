3d55dfcf46ff7477999936cbbeb063e676a020b1
Wintorez7 <liordx76@gmail.com>
2025-04-23 08:26:34 +0530

    creating route and geting single data

--- Sangam-Backend/route-example.js
+++ Sangam-Backend/route-example.js
@@ -8,5 +8,50 @@ app.get('/',(req,res) => {
 });
 
 app.listen(PORT,() => {
-    console.log(`Server Running at ${PORT}`)
+    console.log(`Server is now Running at ${PORT}`)
+})
+
+app.get('/products',(req,res) => {
+    const products = [
+        {
+            id:1,
+            label:'Product 1'
+        },
+        {
+            id:2,
+            label:'Product 2'
+        },
+        {
+            id:3,
+            label:'Product 3'
+        },
+    ]
+     res.json(products);
+   
+})
+
+// get a single products
+app.get('/products/:id',(req,res) => {
+    const productId = parseInt(req.params.id)
+    const products = [
+        {
+            id:1,
+            label:'Product 1'
+        },
+        {
+            id:2,
+            label:'Product 2'
+        },
+        {
+            id:3,
+            label:'Product 3'
+        },
+    ]
+    const getSingleProducts = products.find(product => product.id === productId)
+    if(getSingleProducts){
+        res.json(getSingleProducts)
+    }
+    else{
+        res.status(404).send('Product Not Found! please try with different Id')
+    }
 })
