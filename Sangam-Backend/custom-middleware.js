644def3f5e89fd247c94a3fae8d9c768ce4ac7f6
Wintorez7 <liordx76@gmail.com>
2025-04-23 20:10:24 +0530

    learning Middlewares

---
+++ Sangam-Backend/custom-middleware.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+app.listen(3000,() => {
+    console.log('Server running at port 3000')
+})
+
+const requestTimeStampLogger = (req,res,next) => {
+    const timeStamp = new Date().toISOString();
+    console.log(`${timeStamp} from ${req.method} to ${req.url}`);
+    next();
+}
+
+app.use(requestTimeStampLogger);
+
+app.get('/',(req,res) => {
+    res.send('Home page')
+})
+
+app.get('/about',(req,res) => {
+    res.send('About page')
+})
