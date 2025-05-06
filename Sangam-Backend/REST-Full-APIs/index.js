d0f63882e06aba66213a805eb0ce7c134b82f406
Wintorez7 <liordx76@gmail.com>
2025-05-06 16:55:44 +0530

    learning DELETE Method and Performing CRUD Operation

--- Sangam-Backend/REST-Full-APIs/index.js
+++ Sangam-Backend/REST-Full-APIs/index.js
@@ -52,7 +52,7 @@ app.get('/books',(req,res) => {
 // add a new book
 app.post('/add',(req,res) => {
     const newBook = {
-        id : books.length + 1,
+        id : (books.length + 1).toString(),
         title : `Book ${books.length + 1}`
     }
     books.push(newBook);
@@ -71,6 +71,21 @@ app.put('/update/:id',(req,res) => {
         res.status(200).json({
             message:`Book with ID ${req.params.id} updated succesfully`,
             data : findCurrentBook
+        })
+    }else{
+        res.status(404).json({
+            message:'Book Not Found',
+        })
+    }
+})
+
+app.delete('/delete/:id',(req,res) => {
+    const findIndexOfCurrentBooks = books.findIndex(item => item.id === req.params.id);
+    if(findIndexOfCurrentBooks !== -1){
+        const deletedBook = books.splice(findIndexOfCurrentBooks,1);
+        res.status(200).json({
+            message:'Book Deleted successfully',
+            data: deletedBook[0]
         })
     }else{
         res.status(404).json({
