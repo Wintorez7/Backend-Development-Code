42b12692dd59a3b4955801ec760fd7448234c712
Wintorez7 <liordx76@gmail.com>
2025-05-15 17:55:12 +0530

    working on creating controller

---
+++ Sangam-Backend/Auth-Authorize/controller/auth-controller.js
@@ -0,0 +1,27 @@
+
+
+// register controller
+const registerUser = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:"Some error occured! please try agin"
+        })
+    }
+}
+
+// login controller
+const loginUser = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:"Some error occured! please try agin"
+        })
+    }
+}
