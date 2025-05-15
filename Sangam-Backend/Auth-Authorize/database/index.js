bce3da01fe943eb13573569cfe9cca10e528dcca
Wintorez7 <liordx76@gmail.com>
2025-05-15 17:44:25 +0530

    creating models Schema

---
+++ Sangam-Backend/Auth-Authorize/database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const connectToDb = async () => {
+    try {
+         await mongoose.connect(process.env.MONGO_URL)
+                console.log("MongoDb Connected Succesfully")
+    } catch (error) {
+        console.log("MongoDB Connection Failed",error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectToDb
