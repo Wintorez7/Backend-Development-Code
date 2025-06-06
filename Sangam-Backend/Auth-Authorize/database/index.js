fbadb2d7004c2bc1a25ba0389d02bc702d93283b
Wintorez7 <liordx76@gmail.com>
2025-06-06 16:05:24 +0530

    working auth-authorize

--- Sangam-Backend/Auth-Authorize/database/index.js
+++ Sangam-Backend/Auth-Authorize/database/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const connectToDb = async () => {
     try {
-         await mongoose.connect(process.env.MONGO_URL)
-                console.log("MongoDb Connected Succesfully")
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("MongoDb Connected Succesfully")
     } catch (error) {
         console.log("MongoDB Connection Failed",error)
         process.exit(1)
