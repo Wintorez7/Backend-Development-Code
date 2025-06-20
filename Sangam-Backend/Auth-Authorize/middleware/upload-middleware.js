bf5b8e17478291b6e670a8ec518e9d34913d8a3b
Wintorez7 <liordx76@gmail.com>
2025-06-20 14:20:00 +0530

    working on creating upload middleware

--- Sangam-Backend/Auth-Authorize/middleware/upload-middleware.js
+++ Sangam-Backend/Auth-Authorize/middleware/upload-middleware.js
@@ -1,4 +1,25 @@
 const multer = require('multer')
 const path = require('path')
 
+// set our multer storage
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,
+            file.fiedname + "-" + Date.now() + path.extname(file.originalname)
+        )
+    }
+})
 
+// file filter function
+const checkFilefilter = (req,file,cb) => {
+    if(file.mimetype.startWith('image')){
+        cb(null,true)
+    }else{
+        cb(new Error('Not an Image Please upload only images'))
+    }
+}
+
+//multer middleware
+module.exports = multer({
+   
+})
